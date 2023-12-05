8873c695abedaeddc2f0d31a96aa005efc714d71
Kimyebin00 <kimyeobin12@gmail.com>
2023-12-06 01:14:17 +0900

    programmers coding test lv2: 최솟값 만들기(12941)

---
+++ Lv2_JavaScript/12941.js
@@ -0,0 +1,14 @@
+// 최솟값 만들기 - 정답률 77%
+
+function solution(A,B){
+  let answer = 0;
+
+  const sortedAscA = A.sort((a, b) => a - b);
+  const sortedDesB = B.sort((a, b) => b - a);
+  
+  for(let i = 0; i < A.length; i++) {
+      answer += sortedAscA[i] * sortedDesB[i];
+  }
+
+  return answer;
+}
