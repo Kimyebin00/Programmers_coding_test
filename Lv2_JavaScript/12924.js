dad20f53fe093bca698d1880c55fedd3a8d9c1e6
Kimyebin00 <kimyeobin12@gmail.com>
2023-12-06 14:35:49 +0900

    programmers coding test lv2: 숫자의 표현(12924)

---
+++ Lv2_JavaScript/12924.js
@@ -0,0 +1,20 @@
+// 연습문제 - 숫자의 표현 (정답률 75%)
+
+function solution(n) {
+  let answer = 0;
+
+  for(let i = 1; i < n/2; i++) {
+      let index = i+1;
+      let sum = i;
+      while (sum < n) {
+          sum += index;
+          index++;
+      }
+      if (sum === n) answer++;
+              
+  }
+  
+  answer++;
+  
+  return answer;
+}
