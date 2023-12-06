e949a88f2abdaa50d236ee20d5600bae68315758
Kimyebin00 <kimyeobin12@gmail.com>
2023-12-07 01:22:48 +0900

    programmers coding test lv2: 피보나치 수(12945)

---
+++ Lv2_JavaScript/12945.js
@@ -0,0 +1,24 @@
+// 연습문제 - 피보나치 수 (정답률 73%)
+
+function solution(n) {
+  let answer = 0;
+  
+  let index = 1;
+  let n_1 = 0;
+  let n_2 = 0;
+  while (index < n) {
+      if (index === 1) answer = 1 % 1234567;
+      if (index > 1) {
+          if (index === 2) {
+              n_1 = 1;
+              n_2 = 1;
+          }
+          answer = (n_1 + n_2) % 1234567;
+          n_2 = n_1;
+          n_1 = answer;
+      }
+      index++;
+  }
+      
+  return answer;
+}
